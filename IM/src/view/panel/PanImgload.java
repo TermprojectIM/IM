69c756661d4ea893b6fe2778845cbdff1af407b7
ansgus916 <ansgus916@naver.com>
2016-11-26 22:07:19 +0900

    Frame assist

--- IM/src/view/panel/PanImgload.java
+++ IM/src/view/panel/PanImgload.java
@@ -8,15 +8,15 @@ import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class PanImgload extends JPanel {
-   Image image;
-   public PanImgload(String path){
-      image = Toolkit.getDefaultToolkit().createImage(path);
-   }
-   public void paint(Graphics g){
-      super.paint(g);
-      g.drawImage(image, 0, 0, this);
-   }
-   public void update(Graphics g){
-      super.update(g);
-   }
+	Image image;
+	public PanImgload(String path){
+		image = Toolkit.getDefaultToolkit().createImage(path);
+	}
+	public void paint(Graphics g){
+		super.paint(g);
+		g.drawImage(image, 0, 0, this);
+	}
+	public void update(Graphics g){
+		super.update(g);
+	}
 }
